ce3393a9efd2f569c3ae215a37185abc68506049
Adrien <adrien.dorise@hotmail.com>
2023-02-04 01:07:10 +0100

    Added goal

---
+++ Assets/Scripts/Goal.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goal : MonoBehaviour
+{
+    private List<Transform> goalPos = new List<Transform>();
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i = 0; i < transform.childCount; i++)
+        {
+            goalPos.Add(transform.GetChild(i));
+        }
+
+        transform.Translate(goalPos[Random.Range(0, goalPos.Count)].position);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
