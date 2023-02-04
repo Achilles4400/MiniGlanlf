b1c857d8bdfd92ccc4e4f533b755b09731a0dccc
erwanlecarpentier <erwanlecarpentier@mailbox.org>
2023-02-04 01:11:34 +0100

    Changed raising condition from y pos to time

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,19 +5,27 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public GameObject trunkPiecePrefab;
-    public float raiseHeight;
+    public float raiseDuration; // The trunk grows during raiseDuration seconds
+    public float branchDuration; // The branch grows during branchDuration seconds
     public float raiseVerticalSpeed;
     public float raiseLateralSpeed;
     public float trunkSpawnTimeInterval;
 
-    private float timer = 0;
+    private enum growthState
+    {
+        raise,
+        branch,
+        drop
+    }
+    private float raiseTimer = 0;
+    private float trunkSpawnTimer = 0;
     private float horizontalInput = 0;
     private float verticalInput = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        timer = trunkSpawnTimeInterval;
+        trunkSpawnTimer = trunkSpawnTimeInterval;
     }
 
     // Update is called once per frame
@@ -29,12 +37,15 @@ public class PlayerController : MonoBehaviour
     // Update is called EXACTLY once per frame
     private void FixedUpdate()
     {
-        raiseTree();
+        raisePhase();
+        //branchPhase();
+        //dropPhase();
     }
 
-    private void raiseTree()
+    private void raisePhase()
     {
-        if (transform.position.y < raiseHeight)
+        raiseTimer += Time.fixedDeltaTime;
+        if (raiseTimer < raiseDuration)
         {
             // Get controller key
             horizontalInput = Input.GetAxis("Horizontal");
@@ -46,21 +57,29 @@ public class PlayerController : MonoBehaviour
             transform.Translate(Vector3.forward * Time.fixedDeltaTime * raiseLateralSpeed * verticalInput);
 
             // Spawn trunk piece
-            timer += Time.fixedDeltaTime; // Increment trunk piece spawn timer
-            if (timer >= trunkSpawnTimeInterval)
+            trunkSpawnTimer += Time.fixedDeltaTime;
+            if (trunkSpawnTimer >= trunkSpawnTimeInterval)
             {
                 // Reset trunk piece spawn timer
-                timer = 0;
+                trunkSpawnTimer = 0;
                 // Instantiate trunk piece
                 GameObject newTrunkPiece = Instantiate(
                     trunkPiecePrefab, transform.position, trunkPiecePrefab.transform.rotation);
                 // Rescale new trunk piece
-                float scaleFactor = 0.2f + 0.8f * (1 - transform.position.y / raiseHeight);
+                float scaleFactor = 0.2f + 0.8f * (1 - raiseTimer / raiseDuration);
                 newTrunkPiece.transform.localScale = new Vector3(
                     trunkPiecePrefab.transform.localScale.x * scaleFactor,
                     trunkPiecePrefab.transform.localScale.y,
                     trunkPiecePrefab.transform.localScale.z * scaleFactor);
             }
         }
+    }
+    private void branchPhase()
+    {
+        //horizontalInput = Input.GetAxis("Horizontal");
+    }
+    private void dropPhase()
+    {
+
     }
 }
