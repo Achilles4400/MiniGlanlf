34a3dedd32f0c1519f278dc430a6ba559111dffb
Adrien <adrien.dorise@hotmail.com>
2023-02-05 16:52:44 +0100

    Added water in level1

--- Assets/Scripts/water.cs
+++ Assets/Scripts/water.cs
@@ -11,25 +11,37 @@ public class water : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        player = GameObject.Find("Sphere").transform;
+        player = GameObject.Find("GG").transform;
     }
 
     private void floatingPlayer()
     {
+        player.gameObject.GetComponent<Rigidbody>().drag = 0.01f;
         if (player.position.y - this.transform.position.y < 0)
         {
             player.gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(0f, buoyancy, 0f), ForceMode.Acceleration);
         }
-        player.gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(streamForce, 0f, 0f), ForceMode.Force);
+        player.gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(0f, 0f, -streamForce), ForceMode.Force);
     }
 
     private void OnTriggerStay(Collider other)
     {
-        Debug.Log(other.tag);
         switch(other.tag)
         {
             case "Player":
                 floatingPlayer();
+                break;
+        }
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.tag);
+        switch (other.tag)
+        {
+            case "Player":
+                //player.gameObject.GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 0);
                 break;
         }
 
