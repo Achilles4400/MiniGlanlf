fd2c28b9896293ebb49c13ceee5797b573579bef
erwanlecarpentier <erwanlecarpentier@mailbox.org>
2023-02-05 15:48:48 +0100

    Day-night cycle

---
+++ Assets/Scripts/LightController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightController : MonoBehaviour
+{
+    public GameObject dirLight;
+    public float lightRotationSpeed = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        dirLight.transform.Rotate(Vector3.right * Time.fixedDeltaTime * lightRotationSpeed);
+        //dirLight.GetComponent<Light>().transform.Rotate(Vector3.right * Time.fixedDeltaTime * lightRotationSpeed);
+        //dirLight.GetComponent<Light>().intensity = intensityMagnitude();
+    }
+
+    private float intensityMagnitude()
+    {
+        float xRot = dirLight.transform.rotation.eulerAngles.x;
+        Debug.Log(xRot + " -> " + Mathf.Cos(0.01745329251f * xRot));
+        return (1 + Mathf.Cos(0.01745329251f * xRot)) / 2;
+    }
+}
